82fe76ad0990847441505fee1d17093f677c7e2b
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-03 19:03:18 -0400

    feat: add project basic structure and dependencies

---
+++ src/main/java/com/ctorres/observabilitylab/ObservabilityLabApplication.java
@@ -0,0 +1,13 @@
+package com.ctorres.observabilitylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservabilityLabApplication.class, args);
+    }
+
+}
