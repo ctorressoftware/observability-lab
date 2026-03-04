9f1aff3fa7e76a7cdfdaa9527a8dfe276bfba3ea
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-03 23:57:00 -0400

    feat: create endpoints to simulate latency and use

---
+++ src/main/java/com/ctorres/observabilitylab/controller/WorkController.java
@@ -0,0 +1,46 @@
+package com.ctorres.observabilitylab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/work")
+public class WorkController {
+
+    @GetMapping("/sleep")
+    public String sleep(@RequestParam long seconds) {
+        try {
+            Thread.sleep(seconds);
+            return "I slept like " + (seconds/1000) + " seconds.";
+        } catch (InterruptedException e) {
+            Thread.interrupted();
+            throw new RuntimeException("Thread " + Thread.currentThread().getName()
+                    + " was interrupted", e);
+        }
+    }
+
+    @GetMapping("/cpu")
+    public List<String> cpu(@RequestParam int numberOfIterations) {
+        var iterations = new ArrayList<String>();
+        for (int i = 0; i < numberOfIterations; i++) {
+           iterations.add("Iteration " + (i + 1));
+        }
+        return iterations;
+    }
+
+    @GetMapping("/randomFail")
+    public String randomFail(@RequestParam float failRate) {
+        var random = new Random();
+        float number = random.nextFloat();
+        if (number < failRate) {
+            throw new RuntimeException("It has occurred an controlled error");
+        }
+        return "success!";
+    }
+}
