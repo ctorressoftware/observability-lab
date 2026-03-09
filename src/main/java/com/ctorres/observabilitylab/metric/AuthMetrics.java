47c83f37a4355a3ed6286440139b261a6386f9e5
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-08 22:20:15 -0400

    refactor: change metrics' name

--- src/main/java/com/ctorres/observabilitylab/metric/AuthMetrics.java
+++ src/main/java/com/ctorres/observabilitylab/metric/AuthMetrics.java
@@ -8,18 +8,18 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
-public class WorkMetrics {
+public class AuthMetrics {
     private final MeterRegistry registry;
     private final AtomicInteger loggedUsers = new AtomicInteger(0);
 
-    public WorkMetrics(MeterRegistry registry) {
+    public AuthMetrics(MeterRegistry registry) {
         this.registry = registry;
         registry.gauge("logged_users", loggedUsers);
     }
 
     public void incrementRequests(String endpoint, String result) {
         registry.counter(
-                "work_requests_total",
+                "auth_requests_total",
                 "endpoint", endpoint,
                 "result", result
         ).increment();
@@ -27,7 +27,7 @@ public class WorkMetrics {
 
     public <T> T record(String endpoint, Callable<T> callable) throws Exception {
 
-        Timer timer = Timer.builder("work_duration_seconds")
+        Timer timer = Timer.builder("auth_duration_seconds")
                 .description("Duration of work endpoints")
                 .tag("endpoint", endpoint)
                 .publishPercentileHistogram()
