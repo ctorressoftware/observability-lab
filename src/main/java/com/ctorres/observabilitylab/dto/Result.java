36069f6705dbb97e7f92256048fd58d0041ac2bc
Carlos Torres <ctorressoftware@hotmail.com>
2026-03-14 23:15:11 -0400

    refactor: switch result class to record

--- src/main/java/com/ctorres/observabilitylab/dto/Result.java
+++ src/main/java/com/ctorres/observabilitylab/dto/Result.java
@@ -1,17 +1,6 @@
 package com.ctorres.observabilitylab.dto;
 
-public class Result<T> {
-    private final boolean success;
-    private final String errorCode;
-    private final String errorMessage;
-    private final T data;
-
-    public Result (boolean success, String errorCode, String errorMessage, T data) {
-        this.success = success;
-        this.errorCode = errorCode;
-        this.errorMessage = errorMessage;
-        this.data = data;
-    }
+public record Result<T>(boolean success, String errorCode, String errorMessage, T data) {
 
     public static <T> Result<T> success(T data) {
         return new Result(true, null, null, data);
@@ -19,21 +8,5 @@ public class Result<T> {
 
     public static <T> Result<T> error(String errorCode, String errorMessage) {
         return new Result(false, errorCode, errorMessage, null);
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public T getData() {
-        return data;
     }
 }
